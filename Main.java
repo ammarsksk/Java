6fb04e36c5f2159f08fe2a8cfb939cd6875f8d67
ammarsksk <ammarsaifeek@gmail.com>
2024-09-29 02:24:19 +0530

    YES

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
